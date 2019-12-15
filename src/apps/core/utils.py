6e73a02be3ed89eb17b5fcf3e89914988fe24d13
mnrdev <mnrdev@mnrs-macbook-pro-1.home>
2019-12-15 15:54:40 +0100

    add(core/utils): Get environment variable value.

---
+++ src/apps/core/utils.py
@@ -0,0 +1,25 @@
+# Standard Library
+import os
+
+
+def get_env_variable(var_name):
+    """
+    Get the environment variable or return exception
+
+    Parameters
+    ----------
+    var_name : str
+        Environment variable name.
+
+    Returns
+    -------
+    : str
+        Value of the Environment variable name.ß
+    """
+    try:
+        return os.environ[var_name]
+    except KeyError as e:
+        print(50 * '-')
+        print(f'An error occured attempting to get variable environnement :\n{e}')
+        print(50 * '-')
+        raise (f'Set the {var_name} environment variable.')
