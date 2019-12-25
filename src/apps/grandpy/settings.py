e426cf13cf805bdaf74bc0086fdcf78df654d782
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-25 21:16:04 +0100

    modify(grandpy/settings): key names.

---
+++ src/apps/grandpy/settings.py
@@ -0,0 +1,7 @@
+from apps.core.utils import get_env_variable
+
+GOOGLE_API_SECRET_KEY = get_env_variable('GOOGLE_API_SECRET_KEY')
+GOOGLE_API_KEY_ID = get_env_variable('GOOGLE_API_KEY_ID')
+
+WIKI_API_SECRET_KEY = get_env_variable('WIKI_API_SECRET_KEY')
+WIKI_API_KEY_ID = get_env_variable('WIKI_API_KEY_ID')
