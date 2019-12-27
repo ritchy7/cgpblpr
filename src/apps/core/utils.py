8bd01e016df9f9d1a71f59133b8574cce9a20a92
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-28 00:41:50 +0100

    spelling correction

--- src/apps/core/utils.py
+++ src/apps/core/utils.py
@@ -4,7 +4,7 @@ import os
 
 def get_env_variable(var_name):
     """
-    Get the environment variable or return exception
+    Get the environment variable or return exception.
 
     Parameters
     ----------
@@ -14,7 +14,7 @@ def get_env_variable(var_name):
     Returns
     -------
     : str
-        Value of the Environment variable name.ß
+        Value of the Environment variable name.
     """
     try:
         return os.environ[var_name]
