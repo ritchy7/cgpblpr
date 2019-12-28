883a7815a2d8929189a8a916a1845cb2ebd6ad5f
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-28 02:35:19 +0100

    modify(core/views): static folder , template folder and render the index.html

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='../../static/dist',
+    template_folder='../../static'
+)
 app.config.from_object('config')
 
 
 @app.route('/')
 def index():
-    return "Hello world !"
+    return render_template("index.html")
