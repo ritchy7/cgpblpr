f0b515c2ffd195230d994c1a798d1f3febc3558d
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-28 02:40:54 +0100

    add(index.jsx): commentary.

--- src/static/js/index.jsx
+++ src/static/js/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 require('../css/grandpy.css')
 
-
+// Render the entire React components in root div wich have the root id.
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
