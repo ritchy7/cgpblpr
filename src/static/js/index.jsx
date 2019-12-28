5cca98a4116ccc6170fa04e638cc6c1508ad0ee8
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-28 02:34:54 +0100

    add: front end react file.

---
+++ src/static/js/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+require('../css/grandpy.css')
+
+
+const rootElement = document.getElementById("content");
+ReactDOM.render(<App />, rootElement);
