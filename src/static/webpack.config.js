9ddcb422558867b12023c88c5c3b021d00f11872
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-28 02:33:58 +0100

    add: webpack configuration file.

---
+++ src/static/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+
+const config = {
+    entry: __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+        {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }
+        ]
+    },
+    watch: true,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/
+    },
+};
+
+module.exports = config;
