fcdfe8f558f366fb8c22fba220f1a5c3b44f523c
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-29 00:39:22 +0100

    modify(index.jsx): app to App.

--- src/static/js/index.jsx
+++ src/static/js/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
 require('../css/grandpy.css')
 
 // Render the entire React components in root div wich have the root id.
