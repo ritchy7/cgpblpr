543ca8368c80a257a68a47e825b8e6a97242e7eb
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-28 21:38:14 +0100

    add(footer): Dynamic year.

--- src/static/js/footer.jsx
+++ src/static/js/footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+// Get the current year
+const current_year = new Date().getFullYear()
+
 const Footer = ({ underConstruction }) => (
     <footer>
-        <p>
-            <small>&copy; 2020 Ritchy Blezin </small>
-            <small><a href="#" onClick={underConstruction}>About</a></small>
-        </p>
+        <small>&copy; {current_year} Ritchy Blezin </small>
+        <small><a href="#" onClick={underConstruction}>About</a></small>
     </footer>
 )
 
