b4c7b326a7beb5272b6388b1e623aaf8979c33bf
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-30 00:52:28 +0100

    modify(main.jsx): remove the ternaire.

--- src/static/js/main.jsx
+++ src/static/js/main.jsx
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import MessageForm from "./MessageForm";
 import Message from "./Message";
 
 
 class Main extends Component {
 
+
     render() {
         const { underConstruction, onAddToConversation, conversation } = this.props
         return(
             <div>
                 <main className="msger-chat">
-                    {/* If there a at least one message show them or show
-                    a message inviting you to ask a question. */}
                     {
-                        conversation.length > 1 ?
-                        conversation.map((message) => (<Message message={message}/>))
-                        :
-                        <div style={{textAlign: "center", padding: "20px 0"}}>
-                            Welcome to GrandPY Bot<br /> please ask a question.
-                        </div>
+                        conversation.map(
+                            (message) => <Message key={message.id} message={message}/>
+                        )
                     }
                 </main>
                 <MessageForm
