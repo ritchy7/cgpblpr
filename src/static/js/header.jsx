adac0366397617ce991b0788869ce45ac4883ac7
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-29 18:55:42 +0100

    modify(header): Add a home button on "GrandPY" text header.

--- src/static/js/header.jsx
+++ src/static/js/header.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 
+const grandpy_style = {
+    backgroundColor: "#00C441",
+    borderRadius: "5px",
+    textDecoration: "none",
+    color: "#666666",
+    padding: "5px"
+};
 const Header = ({ underConstruction }) => (
     <header className="msger-header">
         <div className="msger-header-title">
-            <i className="fas fa-comment-alt"></i> GrandPY - Tchat Bot
+            <i className="fas fa-comment-alt">
+                <a style={grandpy_style} href="/">GrandPY</a> - Tchat Bot
+            </i>
         </div>
         <div className="msger-header-options">
             <span><i className="fas fa-cog"></i></span>
