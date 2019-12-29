d732bd082956b3af6de7ec8bfef3e2ad9f57722b
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2019-12-29 01:49:17 +0100

    style(main): add padding.

--- src/static/js/main.jsx
+++ src/static/js/main.jsx
@@ -15,7 +15,7 @@ class Main extends Component {
                 <Message message={message}/>
             ))
         } else {
-            no_message = <p style={{textAlign: "center"}}>Welcome to GrandPY Bot please ask a question.</p>
+            no_message = <p style={{textAlign: "center", padding: "20px 0"}}>Welcome to GrandPY Bot please ask a question.</p>
         }
         return(
             <div>
