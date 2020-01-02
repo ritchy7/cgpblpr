30c0919218f585c289aba656a8c433f4ff91a7b3
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-01-02 21:06:57 +0100

    refactor(Footer): put the function to get the fullyear in the component instead of a variable.

--- src/static/js/Components/Footer.jsx
+++ src/static/js/Components/Footer.jsx
@@ -2,12 +2,9 @@ import React from "react";
 
 
 const Footer = ({ underConstruction }) => {
-    // Get the current year
-    const current_year = new Date().getFullYear()
-
     return (
         <footer>
-            <small>&copy; {current_year} Ritchy Blezin </small>
+            <small>&copy; {new Date().getFullYear()} Ritchy Blezin </small>
             <small><a href="#" onClick={underConstruction}>About</a></small>
             <small><a href="https://github.com/ritchy7/" target="_blank">Github</a></small>
         </footer>
