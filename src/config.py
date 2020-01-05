43c4023bd81e1cd0b4da60012b6d1e04471a9828
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-01-05 20:07:24 +0100

    modify(config): remove WIKI_API variables.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,3 @@
 from apps.core.utils import get_env_variable
 
-GOOGLE_API_SECRET_KEY = get_env_variable('GOOGLE_API_SECRET_KEY')
 GOOGLE_API_KEY_ID = get_env_variable('GOOGLE_API_KEY_ID')
-
-WIKI_API_SECRET_KEY = get_env_variable('WIKI_API_SECRET_KEY')
-WIKI_API_KEY_ID = get_env_variable('WIKI_API_KEY_ID')
