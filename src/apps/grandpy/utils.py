51b49f192c82697bd8112ce380f3c6ef55ffcbf5
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-01-25 02:07:34 +0100

    modify(utils): rework on the totaly way to get a result.

--- src/apps/grandpy/utils.py
+++ src/apps/grandpy/utils.py
@@ -1,38 +1,67 @@
 import ast
 import requests
+from collections import namedtuple
 
 from config import GOOGLE_API_KEY_ID
-from .constants import GOOGLE_API_BASE_URL, STOP_WORDS
+from .constants import GOOGLE_API_BASE_URL_FIND_PLACE, STOP_WORDS
 
 class PlaceInformation:
 
     def __init__(self, sentence):
         self.sentence = sentence
-        self.status = None
-        self.input_message = self.parser_killer()
-        self.parameters = {
-	        "key"   : GOOGLE_API_KEY_ID,
-            "input" : self.input_message
+        self.response = None
+        self.parsed_input_message = self.parser_killer()
+        self.address = self.get_address()
+
+    def call(self, url, parameters):
+        """
+        Make a API Call to the url and the parameters given in parameters.
+
+        Parameters
+        ----------
+        url : str
+            URL where the api call will be sent.
+
+        parameters: dict
+            Parameters to get an accurate result.
+
+        Returns
+        -------
+        response : dict
+            Response with 2 keys candidates which contains the data and status
+            which contains the status result reponse.
+        """
+        response = ast.literal_eval(requests.get(url=url, params=parameters)._content.decode('utf-8'))
+        self.response = response
+        return response
+
+    def get_address(self):
+        """
+        Get an address from a sentence if there are no result remove each word
+        one by one and try to get a result.
+
+        Returns
+        -------
+        response : str
+            Formatted address or empty result.
+        """
+        url = GOOGLE_API_BASE_URL_FIND_PLACE
+        parameters = {
+            'key':GOOGLE_API_KEY_ID,
+            'input': self.parsed_input_message,
+            'inputtype': 'textquery',
+            'fields': 'formatted_address'
         }
-        self.url = GOOGLE_API_BASE_URL
-        self.place_reference = self.get_place_reference()
-
-    def get_place_reference(self):
-        """
-        .
-        """
-        input_message_array = self.input_message.split('+')
-        message_len = len(input_message_array)
-        counter = 0
-        print(input_message_array)
-
-        while True:
-            self.parameters['input'] = self.input_message
-            response = requests.get(url=self.url, params=self.parameters)
-            response = ast.literal_eval(response._content.decode('utf-8'))
-            if response['status'] == "OK":
-                self.status = response['status']
-                self.place_reference = response['predictions'][0]['reference']
+        input_message = self.parsed_input_message.split('+')
+        word_counter = 1
+        while self.call(url, parameters)['status'] == 'ZERO_RESULTS' and word_counter <= len(input_message) / 2:
+            parameters['input'] = '+'.join(input_message[word_counter:])
+            word_counter += 1
+        if self.response['status'] != 'OK':
+            response = ""
+        else:
+            print(self.response)
+            response = self.response['candidates'][0]['formatted_address']
         return response
 
     def parser_killer(self):
@@ -47,9 +76,10 @@ class PlaceInformation:
             structured_sentence - str
                 Sentence parsed (without stop words).
         """
+        self.sentence = self.sentence.lower().replace('\'', ' ')
         structured_sentence = '+'.join([
             word
-            for word in self.sentence.lower().split()
+            for word in self.sentence.split()
             if word not in STOP_WORDS
         ])
 
