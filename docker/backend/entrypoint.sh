db646496af2d6d50074c444db7738e53dd388b37
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-02-21 13:44:09 +0100

    modify(backend/entrypoint): path of the requirements

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pip install -r /usr/src/cgpy/backend/requirements/requirements.txt
+pip install -r /usr/src/cgpy/backend/requirements.txt
 python /usr/src/cgpy/backend/run.py
 exec "$@"
