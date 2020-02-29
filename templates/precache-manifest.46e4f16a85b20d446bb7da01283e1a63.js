f8aaffb8d70cfdd5412aaaae8aeb9ddcc862eb00
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-02-29 19:03:56 +0100

    Deployment

---
+++ templates/precache-manifest.46e4f16a85b20d446bb7da01283e1a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda6f89fba93b4a3088f58999bc9b973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05aa9c24ae411b14020c",
+    "url": "/static/css/main.d55f88c5.chunk.css"
+  },
+  {
+    "revision": "d07bc958a68b006fb806",
+    "url": "/static/js/2.adccc19c.chunk.js"
+  },
+  {
+    "revision": "251cd3629e47e9e82a310119775d7c98",
+    "url": "/static/js/2.adccc19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05aa9c24ae411b14020c",
+    "url": "/static/js/main.02e6168e.chunk.js"
+  },
+  {
+    "revision": "120b21435c83c16d71eb",
+    "url": "/static/js/runtime-main.6934fc53.js"
+  }
+]);
