1a452d0c20dfbc28415f9b21d452e6e017fb32d4
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-03-01 15:16:37 +0100

    modify(MessageForm): the way to clear the message input after submit.

--- frontend/src/Components/MessageForm.js
+++ frontend/src/Components/MessageForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import axios from "axios";
 // Contexts
 import ConversationContext from "../Contexts/ConversationContext";
@@ -9,6 +9,7 @@ const SendMessage = () => {
     const [message, setMessage] = useState("");
     const conversationContextValue = useContext(ConversationContext);
     const loaderContextValue = useContext(LoaderContext);
+    const inputRef = useRef()
 
     const handleChange = event => {
         // Get the written message.
@@ -58,13 +59,14 @@ const SendMessage = () => {
                 loaderContextValue.updateLoaderStatus('hidden')
             })
             .catch((error) => console.log(error));
-        document.querySelector('.message-input').reset();
+        inputRef.current.value = ''
     }
 
     return (
         <div className="message-box">
             <form className="message-input" onSubmit={handleSubmit}>
                 <input
+                    ref={inputRef}
                     onChange={handleChange}
                     type="text"
                     className="message-input"
