30942ad5466b438f78ef7e682420354280905852
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-03-19 19:15:06 +0100

    unittests

--- backend/apps/grandpy/tests/test_utils.py
+++ backend/apps/grandpy/tests/test_utils.py
@@ -46,7 +46,7 @@ class BotResponseTests(TestCase):
         """
         Ensure that the askbot return salut.
         """
-        response = self.tester.post('/askbot', data=json.dumps({'message':'.'}))
+        response = self.tester.post('/askbot', data=json.dumps({'message':''}))
         response = json.loads(response.data.decode('utf-8'))
         self.assertEqual(None, response['position'])
 
@@ -75,7 +75,7 @@ class BotResponseTests(TestCase):
             }],
             'status': 'OK'
         }
-        pi = PlaceInformations('openclassrooms ?')
+        pi = PlaceInformations("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?")
         pi.call = mock.MagicMock(return_value=patched_address_result)
         pi.get_address()
         pi.call = mock.MagicMock(return_value=patched_coordinates_result)
@@ -94,9 +94,70 @@ class BotResponseTests(TestCase):
             }],
             'status': 'OK'
         }
-        pi = PlaceInformations('openclassrooms ?')
+        pi = PlaceInformations("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?")
         pi.call = mock.MagicMock(return_value=patched_address_result)
         self.assertEqual(pi.get_address(), '7 Cité Paradis, 75010 Paris, France')
+
+    def test_get_description(self):
+        """
+        Test the get description method it should return a string with a
+        description of the address or a sentence to tell that the bot
+        doesn't found any description for the address given.
+        """
+        patched_address_result = {
+            'candidates': [{
+                'formatted_address': '7 Cité Paradis, 75010 Paris, France'
+            }],
+            'status': 'OK'
+        }
+        patched_coordinates_result = {
+            'results': [{
+                'geometry': {
+                    'location': {
+                        'lat': 48.8747265,
+                        'lng': 2.3505517
+                    }
+                }
+            }],
+            'status': 'OK'
+        }
+        patched_wiki_pages_result = {
+            "query": {
+                "geosearch": [
+                    {
+                        "pageid":5653202,
+                        "ns":0,
+                        "title":"Cité Paradis",
+                        "lat":48.87409,
+                        "lon":2.35064,
+                        "dist":71.1,
+                        "primary":""
+                    },
+                    {
+                        "pageid":438469,
+                        "ns":0,
+                        "title":"Rue d'Hauteville",
+                        "lat":48.874087,
+                        "lon":2.350645,
+                        "dist":71.4,
+                        "primary":""
+                    }
+                ]
+            }
+        }
+        patched_summary = 'La cité Paradis est une voie publique située dans le 10e arrondissement de Paris'
+        # Init the object.
+        pi = PlaceInformations("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?")
+        # Get the address with a patched result.
+        pi.call = mock.MagicMock(return_value=patched_address_result)
+        pi.get_address()
+        # Get the coordinates with a patched result.
+        pi.call = mock.MagicMock(return_value=patched_coordinates_result)
+        pi.get_coordinates()
+        # Get the description with a patched result.
+        pi.call = mock.MagicMock(return_value=patched_wiki_pages_result)
+        pi.get_summary = mock.MagicMock(return_value=patched_summary)
+        self.assertIn(patched_summary, pi.get_description())
 
     ################
     #   Endpoints
@@ -115,14 +176,24 @@ class BotResponseTests(TestCase):
         response = self.tester.get('/get_google_key', content_type='html/txt')
         response = json.loads(response.data.decode('utf-8'))
         self.assertIsInstance(response['key'], str)
-
-    def test_askbot(self):
+    
+    def test_error_404(self):
         """
-        Ensure that the key api is set.
+        Ensure that the 404 error is returned.
         """
-        response = self.tester.get('/get_google_key', content_type='html/txt')
-        response = json.loads(response.data.decode('utf-8'))
-        self.assertIsInstance(response['key'], str)
+        response = self.tester.get('/page_that_not_exists', content_type='html/txt')
+        self.assertEqual(response.status_code, 404)
+        
+    def test_method_allowed(self):
+        """
+        """
+        endpoints = ['/', '/get_google_key', '/askbot']
+        for endpoint in endpoints:
+            if endpoint == '/askbot':
+                response = self.tester.get(endpoint, content_type='html/txt')
+            else:
+                response = self.tester.post(endpoint)
+            self.assertNotEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
